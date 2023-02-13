09936dce5e29c35fa5372f47a68d2e4d056fa46e
danilo-andjelic <daniloandjelici9@gmail.com>
2023-02-13 20:23:00 +0100

    Comment cleanup, logs added, timeout in config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
-const { token, guildId, welcomeChannelId, introChannelId, rulesChannelId } = require('./config.json');
+const { token, guildId, welcomeChannelId, introChannelId, rulesChannelId, welcomeDelay } = require('./config.json');
 
 const client = new Client({
     intents: [
@@ -14,32 +14,33 @@ const client = new Client({
 });
 
 const timeouts = new Map();
-const welcomeDelay = 180000; // Vreme izvuci u config
+const delay = parseInt(welcomeDelay);
 
 client.on("ready", () => {
-    const guild = client.guilds.cache.get(guildId);
-    welcomeChannel = guild.channels.cache.get(welcomeChannelId);
-    introductionChannel = guild.channels.cache.get(introChannelId);
-    rulesChannel = guild.channels.cache.get(rulesChannelId);
-    // Povuci sve kanale koji postoje
+  const guild = client.guilds.cache.get(guildId);
+  welcomeChannel = guild.channels.cache.get(welcomeChannelId);
+  introductionChannel = guild.channels.cache.get(introChannelId);
+  rulesChannel = guild.channels.cache.get(rulesChannelId);
+  console.log("Now online");
 });
 
 client.on('guildMemberAdd', member => {
     if (member.user.bot) return;
+    console.log(`Countdown started for ${member.user.username}#${member.user.discriminator}`);
     const timeout = setTimeout(() => {
     const embed = new EmbedBuilder()
-    // opcija za naslov isto + mozda ostale
-      .setColor("#5921FF") //boja u config
+      .setColor("#5921FF") 
       .setDescription(`Welcome to the largest Discord Server for the English rock band Muse! Feel free to introduce yourself in ${introductionChannel} and remember to read through ${rulesChannel}. We hope you enjoy your stay!`); // opis u config
-      welcomeChannel.send({content: `Welcome to the server, ${member}!`, embeds: [embed]}); // tekst u config
+      welcomeChannel.send({content: `Welcome to the server, ${member}!`, embeds: [embed]});
     timeouts.delete(member.id);
-  }, welcomeDelay);
+  }, delay);
   timeouts.set(member.id, timeout);
 });
 
 client.on('guildMemberRemove', member => {
   const timeout = timeouts.get(member.id);
   if (timeout) {
+    console.log(`Countdown cancelled for ${member.user.username}#${member.user.discriminator}`);
     clearTimeout(timeout);
     timeouts.delete(member.id);
   }
